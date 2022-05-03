d364903fd7f76e565f2454f5a4a0e78ac801da1d
Sameer Alam <sameeralamynwa@gmail.com>
2022-05-03 18:04:39 +0530

    Update Subarray with Given Sum.cpp

--- Data Structures & Algorithms/Adobe/Subarray with Given Sum.cpp
+++ Data Structures & Algorithms/Adobe/Subarray with Given Sum.cpp
@@ -1,3 +1,5 @@
+Problem: https://practice.geeksforgeeks.org/problems/subarray-with-given-sum-1587115621/1
+
 class Solution{
     public:
     vector<int> subarraySum(int arr[], int n, long long s){
